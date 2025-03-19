d808f05b225dd590b83223ebf2514f702eb31ff4
Elena Khadasevich <camatsu90@mail.ru>
2025-03-20 01:50:20 +0300

    feat: add getCountDaysInMonth

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -132,8 +132,31 @@ function getNextFriday(date) {
  * 1, 2024 => 31
  * 2, 2024 => 29
  */
-function getCountDaysInMonth(/* month, year */) {
-  throw new Error('Not implemented');
+function getCountDaysInMonth(month, year) {
+  let checkYear = false;
+  let numberOfDays = 30;
+  if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+    checkYear = true;
+  }
+
+  if (
+    month === 1 ||
+    month === 3 ||
+    month === 5 ||
+    month === 7 ||
+    month === 8 ||
+    month === 10 ||
+    month === 12
+  ) {
+    numberOfDays = 31;
+  }
+
+  if (month === 2 && checkYear) {
+    numberOfDays = 29;
+  } else if (month === 2) {
+    numberOfDays = 28;
+  }
+  return numberOfDays;
 }
 
 /**
